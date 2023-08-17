4d5913aa332f5820e4e282e9dc636d47b6a7e55f
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-17 16:14:21 +0100

    take input from stdin

---
+++ read_line.c
@@ -0,0 +1,27 @@
+#include "simple_shell.h"
+
+
+char *read_line(void)
+{
+	char *input = NULL;
+	ssize_t bufsize = 0;
+
+	if (getline(&input, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+			exit(EXIT_SUCCESS);
+		else if (!input)
+			fprintf(stderr, "lsh: allocation error\n");
+		else
+		{
+			perror("error in getting the line");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (strcmp(input, "exit\n") == 0)
+	{
+		free(input);
+		exit(0);
+	}
+	return (input);
+}
