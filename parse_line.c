39cacb51f6b96c2ccdc41e45feb8aa9302a3e5dc
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-17 16:13:59 +0100

    remove space in the input

---
+++ parse_line.c
@@ -0,0 +1,28 @@
+#include "simple_shell.h"
+
+/**
+ *parse_the_line-parses the line.
+ * @input:Buffer
+ * Return: vector of words.
+ */
+
+char **parse_the_line(char *input)
+{
+	unsigned int i = 0;
+	char **tokens = (char **) malloc(1024), *token = NULL;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(input, LSH_TOK_DELIM);
+	while (token)
+	{
+		tokens[i] = token;
+    	i++;
+    	token = strtok(NULL, LSH_TOK_DELIM);
+  	}
+	tokens[i] = NULL;
+	return (tokens);
+}
