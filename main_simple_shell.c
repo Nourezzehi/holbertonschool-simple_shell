d18e48a8dc0d4a98a968c82946d5155490cce595
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-17 16:13:31 +0100

    my simple shell

---
+++ main_simple_shell.c
@@ -0,0 +1,26 @@
+#include "simple_shell.h"
+
+/**
+ * main - our shell
+ * @argc: num of arguments
+ * @argv: vector of arguments
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	unsigned int count = 1;
+	char *readline = NULL, **tokens;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		printf("($) ");
+		readline = read_line();
+		tokens = parse_the_line(readline);
+		status = execute_line(tokens);
+		count++;
+	}
+	return (0);
+}
